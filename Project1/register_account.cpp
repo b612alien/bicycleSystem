c56ab33dd13ef275c1ea7ba8f25cbcd4743620c7
b612alien <b612alien@gmail.com>
2025-05-17 23:50:10 +0900

    v3.1

--- Project1/register_account.cpp
+++
@@ -1,6 +0,0 @@
-// register_account.cpp
-#include "register_account.h"
-
-void RegisterAccount::AddNewAccount(string id, string password, string phone) {
-    Account* newAcc = new Account(id, password, phone);
-}
